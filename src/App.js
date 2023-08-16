e39159b13f31a5a035c03cae0494a4e691d3021e
Shivam Kumar <shivamkumar.2121997@gmail.com>
2023-08-16 20:26:58 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { Navbar } from "./components/navbar";
 
 function App() {
   return (
     <div className="App">
-      
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" />
+          <Route path="/cart" />
+        </Routes>
+
+      </Router>
     </div>
   );
 }
