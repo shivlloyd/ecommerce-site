5113f830ee325aea0f18eaeab97b99af3897a631
Shivam Kumar <shivamkumar.2121997@gmail.com>
2023-08-19 19:59:09 +0530

    done

--- src/context/shop-context.jsx
+++ src/context/shop-context.jsx
@@ -14,6 +14,17 @@ const getDefaultCart = () => {
 export const ShopContextProvider = (props) => {
     const [cartItems, setCartItems] = useState(getDefaultCart());
 
+    const getTotalCartAmount = () =>{
+        let totalAmount = 0;
+        for (const item in cartItems){
+            if (cartItems[item] > 0){
+                let itemInfo = PRODUCTS.find((product) => product.id === Number(item));
+                totalAmount += cartItems[item] * itemInfo.price;
+            }
+        }
+        return totalAmount;
+    }
+
     const addToCart = (itemId) => {
         setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1}));
     };
@@ -22,7 +33,17 @@ export const ShopContextProvider = (props) => {
         setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1}));
     };
 
-    const contextValue = {cartItems, addToCart, removeFromCart};
+    const updateCartItemCount = (newAmount, itemId) => {
+        setCartItems((prev) => ({ ...prev, [itemId]: newAmount }));
+    }
+
+    const contextValue = {
+        cartItems, 
+        addToCart, 
+        removeFromCart, 
+        updateCartItemCount, 
+        getTotalCartAmount
+    };
 
   return (
     <ShopContext.Provider value={contextValue}>
