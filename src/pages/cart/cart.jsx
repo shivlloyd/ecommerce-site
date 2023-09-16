f8b1faa934c22532350c24d4c2e1eb4108776998
Shivam Kumar <shivamkumar.2121997@gmail.com>
2023-09-16 14:18:35 +0530

    updated

--- src/pages/cart/cart.jsx
+++ src/pages/cart/cart.jsx
@@ -20,6 +20,7 @@ export const Cart = () => {
           if(cartItems[product.id] !== 0){
             return <CartItem data={product} />
           }
+          return null
         })}
       </div>
       {totalAmount > 0 ? (
